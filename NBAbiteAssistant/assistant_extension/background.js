71c8441782bacd32fac75d68532db356b5bf6514
DALLON HALEY <dallonhaley@gmail.com>
2021-10-24 19:01:22 -0600

    Added ablity to block requests to ad generated cdn

--- NBAbiteAssistant/assistant_extension/background.js
+++ NBAbiteAssistant/assistant_extension/background.js
@@ -8,3 +8,12 @@ chrome.commands.onCommand.addListener(async (command) => {
       files: ['stream.js'],
     });
 });
+
+// //Listens for requests to the js file that generates ads and redirects them to empty js file
+// chrome.webRequest.onBeforeRequest.addListener(
+//   function(details) {
+//       if( details.url == "https://www.cdn4ads.com/intercom.min.js" )
+//           return {redirectUrl: "/intercom.min.js" };
+//   },
+//   {urls: ["*://www.cdn4ads.com/*.js"]},
+//   ["blocking"]);
