b224faf352518a9759a9a4d36f2793570da10a6d
DALLON HALEY <dallonhaley@gmail.com>
2021-10-24 19:23:47 -0600

    Cleaned up code to only include current version

--- NBAbiteAssistant/assistant_extension/background.js
+++ NBAbiteAssistant/assistant_extension/background.js
@@ -8,12 +8,3 @@ chrome.commands.onCommand.addListener(async (command) => {
       files: ['stream.js'],
     });
 });
-
-// //Listens for requests to the js file that generates ads and redirects them to empty js file
-// chrome.webRequest.onBeforeRequest.addListener(
-//   function(details) {
-//       if( details.url == "https://www.cdn4ads.com/intercom.min.js" )
-//           return {redirectUrl: "/intercom.min.js" };
-//   },
-//   {urls: ["*://www.cdn4ads.com/*.js"]},
-//   ["blocking"]);
