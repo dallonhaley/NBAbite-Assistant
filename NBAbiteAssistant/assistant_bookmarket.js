9e4d41adc40f6864722681872890d7d1b0d6d0f2
dallonhaley <47513615+dallonhaley@users.noreply.github.com>
2021-10-24 19:36:15 -0600

    Update assistant_bookmarket.js
    Added comments to clarify what bookmarklet files are for

--- NBAbiteAssistant/assistant_bookmarket.js
+++ NBAbiteAssistant/assistant_bookmarket.js
@@ -1,4 +1,6 @@
 // Get link element that has href starting with 'skip-btn'
+// Bookmarklet can be used as a bookmark in browser to skip straight to the stream
+// Must open bookmarklet on the correct page as defined in the README
 javascript:
 var link = getElementsStartsWithId("skip-btn")[0].href;
 
